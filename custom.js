cc078de3f44f21ef387829ded133edbe6a3658a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-22 00:44:49 +0000

    Deploy balance.tf to github.com/balancetf/balancetf.git:gh-pages

---
+++ custom.js
@@ -0,0 +1,2 @@
+const mainDiv = document.getElementsByTagName("main")[0];
+mainDiv.innerHTML += "<hr><div class='discussion-footer'>Want to be a part of the discussion? Check out <a href='https://discord.gg/7GmCDUP'>the discord server</a>.</div>";
